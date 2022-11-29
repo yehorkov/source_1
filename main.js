f9fd22dd86b4c855bb4950479af86c73c36e5d01
Yehor <102697876+yehorkov@users.noreply.github.com>
2022-11-29 12:43:45 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+import './slider';
+import modals from './modules/modals';
+import tabs from './modules/tabs';
+import forms from './modules/forms'
+import changeModalState from './modules/changeModalState';
+import timer from './modules/timer';
+import images from './modules/images';
+
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    let modalState = { //стандартное значение формы
+        'Форма': 0,
+        'Тип': 'tree'
+    };
+
+    let deadLine = '2022-12-31';
+    
+    changeModalState(modalState);
+    modals();
+    tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
+    tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
+    tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
+    forms(modalState);
+    timer('.timer1', deadLine);
+    images();
+});
